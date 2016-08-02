fbccd21387f9db709279f7afb22caeb67779da7e
Luis Sepulveda <lashidalgo@gmail.com>
2016-08-02 10:30:12 -0400

    Initial project structure, scaffolded with yeoman git add -A

---
+++ server/app.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+/**
+ * Dev
+ */
+if (app.get('env') === 'development') {
+    // This will change in production since we'll be using the dist folder so we only worry about deploying the server folder
+    app.use(express.static(path.join(__dirname, '../client')));
+    // Serving up the index page
+    app.use(express.static(path.join(__dirname, '../client/.tmp')));
+    app.use(express.static(path.join(__dirname, '../client/app')));
+    // Error Handler
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+/**
+ * Production
+ */
+if (app.get('env') === 'production') {
+    // Yeoman will put the optimized files under dist
+    app.use(express.static(path.join(__dirname, '/dist')));
+    // Error handler
+    // no stacktraces shown to user
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    });
+}
+
+
+module.exports = app;
