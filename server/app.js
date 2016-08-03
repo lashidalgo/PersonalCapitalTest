5bdb69e741efc942abb9726f71d5476872c8d282
Luis Sepulveda <lashidalgo@gmail.com>
2016-08-02 20:21:55 -0400

    Back end ready, express server listening POST on / for queryString to response JSON products

--- server/app.js
+++ server/app.js
@@ -4,10 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // uncomment after placing your favicon in /public
@@ -16,42 +13,38 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-/**
- * Dev
- */
-if (app.get('env') === 'development') {
-    // This will change in production since we'll be using the dist folder so we only worry about deploying the server folder
-    app.use(express.static(path.join(__dirname, '../client')));
-    // Serving up the index page
-    app.use(express.static(path.join(__dirname, '../client/.tmp')));
-    app.use(express.static(path.join(__dirname, '../client/app')));
-    // Error Handler
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+app.use('/', routes);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    res.send({
+        message: err.message,
+        error: err
     });
-}
+   return;
+});
 
-/**
- * Production
- */
-if (app.get('env') === 'production') {
-    // Yeoman will put the optimized files under dist
-    app.use(express.static(path.join(__dirname, '/dist')));
-    // Error handler
-    // no stacktraces shown to user
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: {}
-        });
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    res.send({
+        message: err.message,
+        error: {}
     });
-}
-
+   return;
+});
 
 module.exports = app;
